b85208a068c5742ce90862671b36fa399c6238c3
tiandeyang52 <tiandeyang52@163.com>
2018-12-03 20:24:05 +0800

    其他模块

--- eureka-client/src/main/java/com/dytian/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/dytian/eurekaclient/EurekaClientApplication.java
@@ -13,7 +13,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
---
+++ yuemee-order/src/main/java/com/dytian/yuemee/order/YuemeeOrderApplication.java
@@ -0,0 +1,23 @@
+package com.dytian.yuemee.order;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableEurekaClient
+@SpringBootApplication
+@MapperScan("com.dytian.yuemee.order.mapper")
+@ComponentScan("com.dytian.*")
+public class YuemeeOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuemeeOrderApplication.class, args);
+    }
+
+
+
+}
