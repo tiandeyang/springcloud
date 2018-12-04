edde265eae5ff0fabdf031c523baaa410af47695
tiandeyang52 <tiandeyang52@163.com>
2018-12-04 10:31:22 +0800

    yuemee-pro

---
+++ yuemee-pro/src/main/java/com/dytian/yuemee/product/YuemeeProApplication.java
@@ -0,0 +1,12 @@
+package com.dytian.yuemee.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuemeeProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuemeeProApplication.class, args);
+    }
+}
