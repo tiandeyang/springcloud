58e5d357a78a4e8a4e55ba33ce4880ca0305b36e
tiandeyang <tiandeyang52@163.com>
2020-04-09 13:15:50 +0800

    remove the value of accessKeyId
    remove the value of accessKeyId

--- provider-shorttext/src/main/java/com/dytian/yuemee/shorttext/serviceIpl/MsgServiceImpl.java
+++ provider-shorttext/src/main/java/com/dytian/yuemee/shorttext/serviceIpl/MsgServiceImpl.java
@@ -32,7 +32,7 @@ public class MsgServiceImpl implements MsgService {
 		final String product = "Dysmsapi";//短信API产品名称（短信产品名固定，无需修改）
 		final String domain = "dysmsapi.aliyuncs.com";//短信API产品域名（接口地址固定，无需修改）
 //替换成你的AK
-		final String accessKeyId = "bakLTAIbakQAiWQ9wbhGtZ";//你的accessKeyId,
+		final String accessKeyId = "";//你的accessKeyId,
 		final String accessKeySecret = "";//你的accessKeySecret，参考本文档步骤2
 //初始化ascClient,暂时不支持多region（请勿修改）
 		IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId,
